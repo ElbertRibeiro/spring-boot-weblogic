1bd211f7a7306b6b65d857ced8abf121f18d80f7
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-05-27 00:12:31 -0300

    adicionando servico nas dependencias

--- spring-boot-weblogic-api/src/main/java/com/elbertribeiro/controller/TesteController.java
+++ spring-boot-weblogic-api/src/main/java/com/elbertribeiro/controller/TesteController.java
@@ -1,5 +1,6 @@
 package com.elbertribeiro.controller;
 
+import com.elbertribeiro.teste.TesteService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TesteController {
     @GetMapping
     public String getTest() {
-        return "Teste Weblogic com multiModules";
+        return new TesteService().retornoService();
     }
 }
---
+++ spring-boot-weblogic-core/src/main/java/com/elbertribeiro/teste/TesteService.java
@@ -0,0 +1,8 @@
+package com.elbertribeiro.teste;
+
+
+public class TesteService {
+    public String retornoService(){
+        return "Teste Weblogic com multiModules e service";
+    }
+}
