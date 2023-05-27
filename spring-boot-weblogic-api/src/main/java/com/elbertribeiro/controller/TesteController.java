a15d3fa2f397629debcd3cc82953dbc622f872e1
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-05-27 00:23:11 -0300

    adicionando servico dependencias para anotations

--- spring-boot-weblogic-api/src/main/java/com/elbertribeiro/controller/TesteController.java
+++ spring-boot-weblogic-api/src/main/java/com/elbertribeiro/controller/TesteController.java
@@ -1,6 +1,7 @@
 package com.elbertribeiro.controller;
 
 import com.elbertribeiro.teste.TesteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("teste")
 public class TesteController {
+    @Autowired
+    private TesteService service;
+
     @GetMapping
     public String getTest() {
-        return new TesteService().retornoService();
+        return service.retornoService();
     }
 }
